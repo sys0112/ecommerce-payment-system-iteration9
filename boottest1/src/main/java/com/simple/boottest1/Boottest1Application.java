c98c6bfb552be5679a07e0428324df51a0d55858
tlstjr8323 <tlstjr8323@naver.com>
2024-07-02 21:28:55 +0900

    upload

---
+++ boottest1/src/main/java/com/simple/boottest1/Boottest1Application.java
@@ -0,0 +1,13 @@
+package com.simple.boottest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boottest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boottest1Application.class, args);
+	}
+
+}
---
+++ boottest1/src/test/java/com/simple/boottest1/Boottest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.boottest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boottest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
